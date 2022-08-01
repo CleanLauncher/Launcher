Scrumplex/PrismLauncher

feat: display the 'body' of a MR mod on the mod downloader
Signed-off-by: flow <flowlnlnln@gmail.com>

launcher/ui/pages/modplatform/ModPage.cpp
@@ -40,6 +40,8 @@
 #include <QKeyEvent>
 #include <memory>
 
+#include <HoeDown.h>
+
 #include "minecraft/MinecraftInstance.h"
 #include "minecraft/PackProfile.h"
 #include "ui/dialogs/ModDownloadDialog.h"
@@ -288,5 +290,6 @@ void ModPage::updateUi()
 
     text += "<hr>";
 
-    ui->packDescription->setHtml(text + current.description);
+    HoeDown h;
+    ui->packDescription->setHtml(text + (current.extraData.body.isEmpty() ? current.description : h.process(current.extraData.body.toLatin1())));
 }

CleanLauncher/Launcher

launcher/ui/pages/modplatform/ModPage.cpp
@@ -40,6 +40,8 @@
 #include <QKeyEvent>
 #include <memory>
 
+#include <HoeDown.h>
+
 #include "minecraft/MinecraftInstance.h"
 #include "minecraft/PackProfile.h"
 #include "ui/dialogs/ModDownloadDialog.h"
@@ -288,5 +290,6 @@ void ModPage::updateUi()
 
     text += "<hr>";
 
-    ui->packDescription->setHtml(text + current.description);
+    HoeDown h;
+    ui->packDescription->setHtml(text + (current.extraData.body.isEmpty() ? current.description : h.process(current.extraData.body.toUtf8())));
 }