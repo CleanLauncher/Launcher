TheKodeToad/PrismLauncher

fix: take JavaCheckResult by const reference
Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

launcher/JavaCommon.cpp
@@ -63,7 +63,7 @@ bool JavaCommon::checkJVMArgs(QString jvmargs, QWidget* parent)
     return true;
 }
 
-void JavaCommon::javaWasOk(QWidget* parent, JavaCheckResult result)
+void JavaCommon::javaWasOk(QWidget* parent, const JavaCheckResult& result)
 {
     QString text;
     text += QObject::tr(
@@ -79,7 +79,7 @@ void JavaCommon::javaWasOk(QWidget* parent, JavaCheckResult result)
     CustomMessageBox::selectable(parent, QObject::tr("Java test success"), text, QMessageBox::Information)->show();
 }
 
-void JavaCommon::javaArgsWereBad(QWidget* parent, JavaCheckResult result)
+void JavaCommon::javaArgsWereBad(QWidget* parent, const JavaCheckResult& result)
 {
     auto htmlError = result.errorLog;
     QString text;
@@ -89,7 +89,7 @@ void JavaCommon::javaArgsWereBad(QWidget* parent, JavaCheckResult result)
     CustomMessageBox::selectable(parent, QObject::tr("Java test failure"), text, QMessageBox::Warning)->show();
 }
 
-void JavaCommon::javaBinaryWasBad(QWidget* parent, JavaCheckResult result)
+void JavaCommon::javaBinaryWasBad(QWidget* parent, const JavaCheckResult& result)
 {
     QString text;
     text += QObject::tr(

launcher/JavaCommon.h
@@ -10,11 +10,11 @@ namespace JavaCommon {
 bool checkJVMArgs(QString args, QWidget* parent);
 
 // Show a dialog saying that the Java binary was usable
-void javaWasOk(QWidget* parent, JavaCheckResult result);
+void javaWasOk(QWidget* parent, const JavaCheckResult& result);
 // Show a dialog saying that the Java binary was not usable because of bad options
-void javaArgsWereBad(QWidget* parent, JavaCheckResult result);
+void javaArgsWereBad(QWidget* parent, const JavaCheckResult& result);
 // Show a dialog saying that the Java binary was not usable
-void javaBinaryWasBad(QWidget* parent, JavaCheckResult result);
+void javaBinaryWasBad(QWidget* parent, const JavaCheckResult& result);
 // Show a dialog if we couldn't find Java Checker
 void javaCheckNotFound(QWidget* parent);
 

CleanLauncher/Launcher

launcher/JavaCommon.cpp
@@ -68,7 +68,7 @@ bool JavaCommon::checkJVMArgs(QString jvmargs, QWidget *parent)
     return true;
 }
 
-void JavaCommon::javaWasOk(QWidget *parent, JavaCheckResult result)
+void JavaCommon::javaWasOk(QWidget *parent, const JavaCheckResult &result)
 {
     QString text;
     text += QObject::tr("Java test succeeded!<br />Platform reported: %1<br />Java version "
@@ -83,7 +83,7 @@ void JavaCommon::javaWasOk(QWidget *parent, JavaCheckResult result)
     CustomMessageBox::selectable(parent, QObject::tr("Java test success"), text, QMessageBox::Information)->show();
 }
 
-void JavaCommon::javaArgsWereBad(QWidget *parent, JavaCheckResult result)
+void JavaCommon::javaArgsWereBad(QWidget *parent, const JavaCheckResult &result)
 {
     auto htmlError = result.errorLog;
     QString text;
@@ -93,7 +93,7 @@ void JavaCommon::javaArgsWereBad(QWidget *parent, JavaCheckResult result)
     CustomMessageBox::selectable(parent, QObject::tr("Java test failure"), text, QMessageBox::Warning)->show();
 }
 
-void JavaCommon::javaBinaryWasBad(QWidget *parent, JavaCheckResult result)
+void JavaCommon::javaBinaryWasBad(QWidget *parent, const JavaCheckResult &result)
 {
     QString text;
     text += QObject::tr(

launcher/JavaCommon.h
@@ -11,11 +11,11 @@ namespace JavaCommon
     bool checkJVMArgs(QString args, QWidget *parent);
 
     // Show a dialog saying that the Java binary was usable
-    void javaWasOk(QWidget *parent, JavaCheckResult result);
+    void javaWasOk(QWidget *parent, const JavaCheckResult &result);
     // Show a dialog saying that the Java binary was not usable because of bad options
-    void javaArgsWereBad(QWidget *parent, JavaCheckResult result);
+    void javaArgsWereBad(QWidget *parent, const JavaCheckResult &result);
     // Show a dialog saying that the Java binary was not usable
-    void javaBinaryWasBad(QWidget *parent, JavaCheckResult result);
+    void javaBinaryWasBad(QWidget *parent, const JavaCheckResult &result);
     // Show a dialog if we couldn't find Java Checker
     void javaCheckNotFound(QWidget *parent);
 