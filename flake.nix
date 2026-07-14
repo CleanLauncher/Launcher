c2dd154d9b7b741c26e433c392f5bf09d66f1a79
Alessio Attilio <attilio.alessio@protonmail.com>
2026-07-14 22:05:57 +0200

    ci: fix signoff, spellcheck, nix formatter, add treefmt config
    Signed-off-by: Alessio Attilio <attilio.alessio@protonmail.com>

--- flake.nix
+++ flake.nix
@@ -4,17 +4,20 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    treefmt-nix,
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
         launcher = pkgs.callPackage ./nix/unwrapped.nix {};
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
       in {
         packages = {
           launcher = pkgs.callPackage ./nix/wrapper.nix {inherit launcher;};
@@ -27,6 +30,8 @@
         };
 
         checks.launcher = self.packages.${system}.launcher;
+
+        formatter = treefmtEval.config.program;
 
         devShells.default = pkgs.mkShell {
           inputsFrom = [self.packages.${system}.launcher];
