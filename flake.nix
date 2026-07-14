e2ee27994394a0ca871fc0a7e466d205f48dd20a
Alessio Attilio <attilio.alessio@protonmail.com>
2026-07-14 20:58:56 +0200

    ci: fix validate/nix/clang-tidy, remove container workflow
    Signed-off-by: Alessio Attilio <attilio.alessio@protonmail.com>

--- flake.nix
+++ flake.nix
@@ -6,26 +6,32 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = nixpkgs.legacyPackages.${system};
-        launcher = pkgs.callPackage ./nix/unwrapped.nix { };
+        launcher = pkgs.callPackage ./nix/unwrapped.nix {};
       in {
         packages = {
-          launcher = pkgs.callPackage ./nix/wrapper.nix { inherit launcher; };
-          launcher-debug = pkgs.callPackage ./nix/wrapper.nix {
-            inherit launcher;
-            debug = true;
-          };
+          launcher = pkgs.callPackage ./nix/wrapper.nix {inherit launcher;};
+          launcher-debug =
+            pkgs.callPackage ./nix/wrapper.nix {
+              inherit launcher;
+              debug = true;
+            };
           default = self.packages.${system}.launcher;
         };
 
         checks.launcher = self.packages.${system}.launcher;
 
         devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.launcher ];
-          packages = with pkgs; [ cmake ninja ];
+          inputsFrom = [self.packages.${system}.launcher];
+          packages = with pkgs; [cmake ninja];
         };
-      });
+      }
+    );
 }
